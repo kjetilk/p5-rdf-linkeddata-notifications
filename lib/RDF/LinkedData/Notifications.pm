912e8a2c59db90549a658d5e5a047738dcdb0526
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-09-22 18:22:38 +0200

    Stub distro

---
+++ lib/RDF/LinkedData/Notifications.pm
@@ -0,0 +1,50 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package RDF::LinkedData::Notifications;
+
+our $AUTHORITY = 'cpan:KJETILK';
+our $VERSION   = '0.001';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+RDF::LinkedData::Notifications - Experimental implementation of the Linked Data Notifications
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=RDF-LinkedData-Notifications>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo E<lt>kjetilk@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2016 by Kjetil Kjernsmo.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
